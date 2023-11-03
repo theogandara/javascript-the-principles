60d54f403b8c2b348117de5a1387e81b17b9f3cd
Théo Gândara <theo.gandara@hexalab.com.br>
2023-11-03 23:23:21 +0000

    Feat: dataTypes

---
+++ dataTypes.js
@@ -0,0 +1,17 @@
+// Primitives or Objects
+
+// Primitives
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol
+
+// Objects
+// 1. Object
+// 2. Function
+// 3. Array
+// 4. Date
+// 5. RegExp
+// 6. Error
