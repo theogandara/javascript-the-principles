0dee5a7eef9b8f4aad06991ba00ad56aad3f162d
Théo Gândara <theo.gandara@hexalab.com.br>
2023-11-04 13:21:47 +0000

    Feat: stoped in Numbers class

--- Number/number.js
+++ Number/number.js
@@ -1,11 +1,13 @@
-// binary representations
+// Representated by IEEE 754 standard (64 bits)
+
+// binary representations - 0b 0B
 const binaryOf42 = 0b101010;
 console.log(binaryOf42);
 
-// octal representations
+// octal representations - 0o 0O
 const octalOf42 = 0o52;
 console.log(octalOf42);
 
-// hexadecimal representations
+// hexadecimal representations - 0x 0X
 const hexaOf255 = 0xFF;
 console.log(hexaOf255);
